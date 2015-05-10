9198e1db0f28803c9c26da8b66a2ef23818c2006
Aleksandra Majchrzak <aleksandra.a.majchrzak@gmail.com>
2015-05-10 20:18:21 +0000

    Add first_name and last_name columns to user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,14 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  :recoverable, :rememberable, :trackable, :validatable, :timeoutable
+  
+  validates :first_name, presence: true
+  validates :email, presence: true
+  
+  def  full_name
+    "#{first_name} #{last_name}"
+    #[first_name, last_name].join(" ")
+  end
+  
 end
