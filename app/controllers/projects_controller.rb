650cd55354311c930b3e94b67d39bce80cebee46
Aleksandra Majchrzak <aleksandra.a.majchrzak@gmail.com>
2015-05-12 12:37:19 +0000

    Added functions to TicketController and corrected project_params in ProjectsController.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -44,7 +44,7 @@ class ProjectsController < ApplicationController
   
   private
   def project_params
-    params.require(:project).permit(:name, :text)
+    params.require(:project).permit(:name, :description)
   end
   
 end
--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,2 +1,54 @@
 class TicketsController < ApplicationController
+  def index
+      @project= Project.find(params[:project_id])
+      @ticket=@project.tickets.all
+  end
+  
+  def new
+    @project=Project.find(params[:project_id])
+    @ticket=@project.tickets
+  end
+  
+  def create
+    @project= Project.find(params[:project_id])
+    @ticket = @project.tickets.new(ticket_params)
+ 
+    if @ticket.save
+      redirect_to @ticket
+    else
+      render 'new'
+    end
+  end
+  
+  def show
+    @project= Project.find(params[:project_id])
+    @ticket=@project.tickets.find(params[:id])
+  end
+  
+  def destroy
+    @project= Project.find(params[:project_id])
+    @ticket=@project.tickets.find(params[:id])
+    @ticket.destroy
+    
+    redirect_to project_tickets_path
+  end
+  
+  def edit
+    @ticket=Ticket.find(params[:id])
+  end
+  
+  def update
+    @ticket = Ticket.find(params[:id])
+ 
+    if @ticket.update(ticket_params)
+      redirect_to @ticket
+    else
+      render 'edit'
+    end
+  end
+  
+  private
+  def ticket_params
+    params.require(:ticket).permit(:name, :description, :priority, :difficulty, :status)
+  end
 end
