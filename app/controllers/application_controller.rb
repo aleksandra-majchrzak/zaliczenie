1a856db86c107e67e7fbd90246e86cb7ddf26314
Aleksandra Majchrzak <aleksandra.a.majchrzak@gmail.com>
2015-05-16 09:14:09 +0000

    Deleted unneeded profile controller, views and paths

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,8 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :first_name, :last_name) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :first_name, :last_name,           
+                                                                                                                            :current_password) }
   end
 
   def after_sign_in_path_for(resource)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'profile/show'
-  get 'profile/edit'
 
   devise_for :users
   get 'welcome/index'
