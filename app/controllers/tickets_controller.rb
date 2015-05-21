795b1bb85d291eecb1bf1817074073172119ba56
Aleksandra Majchrzak <aleksandra.a.majchrzak@gmail.com>
2015-05-21 21:18:28 +0000

    Changed authenticate_user statement.

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,5 +1,5 @@
 class TicketsController < ApplicationController
-  before_filter :authenticate_user!, except:[:index, :show]
+  before_filter :authenticate_user!
   load_and_authorize_resource
   
   def index
