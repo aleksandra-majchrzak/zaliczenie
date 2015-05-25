ee3d4810b055c9955798d16134d25b553741b95c
Aleksandra Majchrzak <aleksandra.a.majchrzak@gmail.com>
2015-05-25 20:27:28 +0000

    Changed users abilities.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,8 +5,10 @@ class Ability
     # Define abilities for the passed in user here. For example:
     #
     user ||= User.new # guest user (not logged in)
+    can :create, Project
     can :manage, Project, :user_id => user.id 
-    can :manage, Ticket, :project => {:user_id => user.id}
+    can :create, Ticket
+    can :manage, Ticket, :project => {:user_id => user.id}    
        #else
     can :read, :all
       # end
